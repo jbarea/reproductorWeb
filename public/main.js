7ced4cc3eba5348d256ddd58187e226be00a2a23
Juan Antonio <jbarealopez2@gmail.com>
2018-06-29 13:47:28 +0200

    Commit inicial con estructura principal

---
+++ public/main.js
@@ -0,0 +1,19 @@
+SC.initialize({
+    client_id: 'unnFdubicpq7RVFFsQucZzduDPQTaCYy'
+})
+
+function miOnDragStart(){
+
+}
+
+function miOnDragEnd(){
+
+}
+
+function miOnDragOver(){
+
+}
+
+function miOnDrop(){
+    
+}
