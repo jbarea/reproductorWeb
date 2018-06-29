a941027857a4ac3a753aa10f73f93cf887f69eac
Juan Antonio <jbarealopez2@gmail.com>
2018-06-29 18:16:52 +0200

    Añadida funcionalidad de busqueda, pero sin drag and drop

--- public/main.js
+++ public/main.js
@@ -2,6 +2,34 @@ SC.initialize({
     client_id: 'unnFdubicpq7RVFFsQucZzduDPQTaCYy'
 })
 
+//let data = document.getElementById('busqueda').value;
+var page_size = 10;
+
+function getInfo(){
+    data = document.getElementById('busqueda').value;
+    console.log(data);
+    SC.get('/tracks', {
+        limit: page_size,
+        q: `${data}`,
+    }).then(function (tracks) {
+        //var arrayData = [];
+        var zonaFoto = document.getElementById('resultado');
+        console.log(tracks);
+        //console.log(tracks[1]['artwork_url']);
+        for (var ele of tracks){
+            //arrayData.push(ele['artwork_url']);
+            var elemento = document.createElement('img');
+                if (ele['artwork_url']!= null){
+                    elemento.src = ele['artwork_url'];
+                    elemento.draggable = true;
+                    zonaFoto.appendChild(elemento);
+                    console.log(elemento);
+                } 
+        }
+        //console.log(arrayData);
+    });
+}
+
 function miOnDragStart(){
 
 }
@@ -15,5 +43,5 @@ function miOnDragOver(){
 }
 
 function miOnDrop(){
-    
+
 }
