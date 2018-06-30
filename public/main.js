35588f2e3e78baa88d17deb4ecc23b3c022ed6a7
Juan Antonio <jbarealopez2@gmail.com>
2018-06-30 14:34:28 +0200

    Empezando a implementar drag and drop

--- public/main.js
+++ public/main.js
@@ -30,18 +30,34 @@ function getInfo(){
     });
 }
 
-function miOnDragStart(){
+// SC.stream('/tracks/88295642').then(function (player) {
+//     player.play();
+// }).catch(function (error) {
+//     console.error('NO ha funcionado la peticion');
+// })
 
+function miOnDragStart(ev){
+    //console.log(event.target.id);
+    ev.dataTransfer.setData("targetId", event.target.id);
+    console.log('onDragStart');
+    console.log(ev)
 }
 
-function miOnDragEnd(){
-
+function miOnDragEnd(ev){
+    console.log("onDragEnd");
+    console.log(ev.target);
 }
 
-function miOnDragOver(){
+function miOnDragOver(ev){
+    ev.preventDefault;
+    console.log("onDragOver");
 
 }
 
 function miOnDrop(){
-
+    ev.preventDefault();//para que en mozilla no falle y añada una imagen en tab nuevo
+    //var data = ev.dataTransfer.getData("text");
+    var data = ev.dataTransfer.getData("targetId");
+    ev.target.appendChild(document.getElementById(data));//para llamar a si mismo y modificar el documento|estamos dejando la imagen dentro del div
+    console.log('onDrop', data)
 }
